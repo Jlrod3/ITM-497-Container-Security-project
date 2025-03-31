5172712f040b16ecdd5ef83104742296c21615c3
Jlrod3 <jrodriguez31@hawk.iit.edu>
2025-03-31 15:23:41 -0500

    adding server port and server error handling

--- helloWorld.go
+++ helloWorld.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+
+	port := ":8069"
+	fmt.Printf("Server starting on port %s...\n", port)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
 }
